02a3c020af0c7945e317372e39cb846693708159
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2024-09-08 00:05:14 -0300

    Add components.json and update tailwind.config.ts
    This commit adds the components.json file and updates the tailwind.config.ts file. The components.json file includes configuration for the UI components, such as the style, aliases, and tailwind settings. The tailwind.config.ts file is updated to extend the theme colors and add new variants for the UI components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,114 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { ImagePlus, ImageUp } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [file, setFile] = useState<File | null>(null);
+  const [prediction, setPrediction] = useState<string | null>(null);
+  const [image, setImage] = useState<string | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrediction(null);
+    if (!e.target.files) return;
+
+    if (e.target.files.length === 0) {
+      setImage(null);
+      return;
+    }
+    setFile(e.target.files[0]);
+    setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const sendFile = async () => {
+    setPrediction(null);
+
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+    const res = await fetch("http://localhost:5000/predict", {
+      method: "POST",
+      body: formData,
+      cache: "no-cache",
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setPrediction(data.prediction);
+    } else {
+      console.error("Error");
+    }
+  };
+
+  return (
+    <div className="justify-items-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <h1 className="text-4xl font-bold">Clock Test</h1>
+
+        <motion.div animate={{ opacity: [0, 1] }} className="flex gap-8">
+          <p className="text-lg max-w-sm">
+            Draw a clock on a piece of paper like the example below and upload
+            it.
+          </p>
+          <Image src="/clock.png" width={200} height={200} alt="clock" />
+        </motion.div>
+        <div className="flex gap-4 items-center justify-center">
+          <ImagePlus />
+
+          <Input
+            type="file"
+            accept="image/*"
+            placeholder="Upload Image"
+            onChange={(e) => {
+              handleFileChange(e);
+            }}
+            className="w-fit"
+          />
+        </div>
+        <div className="flex justify-between text-center w-full max-w-lg">
+          <motion.div
+            animate={{ scale: [0, 1] }}
+            className="flex flex-col gap-4"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {image && (
+              <motion.div animate={{ scale: [0, 1] }}>
+                <Image src={image} width={200} height={200} alt="clock" />
+              </motion.div>
+            )}
+            {file && (
+              <Button
+                className="flex gap-2"
+                variant={"secondary"}
+                onClick={() => sendFile()}
+              >
+                <ImageUp />
+                <p>Upload</p>
+              </Button>
+            )}
+          </motion.div>
+          {prediction && (
+            <motion.div
+              animate={{ scale: [0, 1] }}
+              className="flex flex-col gap-4 items-center justify-center"
+            >
+              <h1 className="text-2xl font-bold">Result:</h1>
+              <h2
+                className={`
+                  text-2xl font-bold
+                  ${
+                    prediction === "disorder" ? "text-red-400" : "text-blue-400"
+                  }
+                  `}
+              >
+                {prediction}
+              </h2>
+            </motion.div>
+          )}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
